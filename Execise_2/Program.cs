6fff09de37576c19233c9f8fa79bcf2b01845ded
Ann <annakuznetsova419@gmail.com>
2022-10-21 15:37:45 +1000

    Execise_2

---
+++ Execise_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число №1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число №2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2) 
+{
+    Console.WriteLine($"Число №1, равное {number1}, больше");
+}
+else
+{
+    Console.WriteLine($"Число №2, равное {number2}, больше");
+}

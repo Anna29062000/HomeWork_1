4c456e75ce28b6362ac39f0474f5bfffabe2e120
Ann <annakuznetsova419@gmail.com>
2022-10-21 15:40:54 +1000

    Exercise_8

---
+++ Execise_8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int number = 2;
+
+while (number <= n)
+{
+    if (number%2 == 0)
+    {
+        Console.WriteLine(number);
+    }
+    number = number + 1;
+}

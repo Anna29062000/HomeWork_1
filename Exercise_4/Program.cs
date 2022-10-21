9c5a867dd627f0c2ae134cb4a1eb8cf480842a86
Ann <annakuznetsova419@gmail.com>
2022-10-21 15:44:47 +1000

    Exercise4

---
+++ Exercise_4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число №1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число №2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число №3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (max < number2) 
+{
+    max = number2;
+}
+
+if (max < number3) 
+{
+    max = number3;
+}
+Console.WriteLine($"Наиболешее число: {max}");
